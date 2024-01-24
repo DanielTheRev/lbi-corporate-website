ecd5411726dc019065d43a86542497a1d7dc81d0
Daniel Larrosa <larrosadaniel2894@gmail.com>
2024-01-24 03:07:54 -0300

    Optimizando Responsive UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<head>
 				<link rel='icon' href='/favicon.ico' sizes='any' />
 			</head>
-			<body className={'relative overflow-x-hidden ' + Noto_S.className}>
+			<body className={'relative flex flex-col min-h-screen justify-between overflow-x-hidden ' + Noto_S.className}>
 				<div className='absolute top-0 z-[-2] h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
 
 				<Navbar />
-				<main className='flex flex-col w-full h-auto justify-between'>{children}</main>
+				<main className='flex flex-col w-full h-full justify-between'>{children}</main>
 				<Footer />
 			</body>
 		</html>
