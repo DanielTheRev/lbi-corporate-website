1df0a57242e57779badc0bb924b6320b4e1b70a5
Daniel Larrosa <larrosadaniel2894@gmail.com>
2024-01-29 22:15:44 -0300

    UI: Navbar resposive optimizado

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: React.FC<Props> = ({ Routes }) => {
 			className='flex w-full max-w-[1200px] mx-auto justify-center items-center overflow-x-hidden'
 			id='navbar'>
 			<nav className='w-full'>
-				<div className='flex flex-wrap items-center justify-between mx-auto p-2 md:p-4'>
+				<div className='flex flex-wrap items-center justify-between mx-auto p-2 lg:p-4'>
 					<Link
 						href='/'
 						className='flex items-center space-x-3 text-white rtl:space-x-reverse'>
@@ -34,9 +34,7 @@ const Navbar: React.FC<Props> = ({ Routes }) => {
 					<button
 						type='button'
 						onClick={() => setShowMenu((state) => !state)}
-						className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-						aria-controls='navbar-default'
-						aria-expanded='false'>
+						className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
 						<span className='sr-only'>Abrir menu</span>
 						<svg
 							className='w-5 h-5'
@@ -56,15 +54,15 @@ const Navbar: React.FC<Props> = ({ Routes }) => {
 					<div
 						className={`${
 							showMenu ? 'block' : 'hidden'
-						} w-full md:block md:w-auto animate-fade-left animate-duration-[500ms] md:animate-none`}>
-						<ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700'>
+						} w-full lg:block lg:w-auto animate-fade-left animate-duration-[500ms] lg:animate-none`}>
+						<ul className='font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-white dark:bg-transparent lg:dark:bg-transparent dark:border-gray-700'>
 							<li>
 								<Link
 									href='/'
 									className={`block ${
 										pathName === '/' &&
-										'bg-blue-700 md:bg-transparent md:dark:text-blue-500 dark:text-white'
-									} py-2 px-3 text-white  rounded  md:p-0 `}
+										'bg-blue-700 lg:bg-transparent lg:dark:text-blue-500 dark:text-white'
+									} py-2 px-3 text-white  rounded  lg:p-0 `}
 									aria-current='page'>
 									Home
 								</Link>
@@ -75,8 +73,8 @@ const Navbar: React.FC<Props> = ({ Routes }) => {
 										href={route}
 										className={`block ${
 											pathName.startsWith(route) &&
-											'bg-blue-700 md:bg-transparent md:dark:text-blue-500'
-										} py-2 px-3 text-gray-900 rounded hover:bg-gray-100  md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>
+											'bg-blue-700 lg:bg-transparent lg:dark:text-blue-500'
+										} py-2 px-3 text-gray-900 rounded hover:bg-gray-100  lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent`}>
 										{label}
 									</Link>
 								</li>
